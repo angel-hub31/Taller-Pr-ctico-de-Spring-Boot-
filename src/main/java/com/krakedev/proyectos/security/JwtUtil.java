04938c73913303dc3a5f0785be8093f1d0ae245b
angel-hub31 <moralesmales1@hotmail.com>
2026-06-08 19:01:46 -0500

    se crea la clase JwtUtil dentro del paquete proyectos.security

--- src/main/java/com/krakedev/proyectos/security/JwtUtil.java
+++ src/main/java/com/krakedev/proyectos/security/JwtUtil.java
@@ -1,5 +1,29 @@
 package com.krakedev.proyectos.security;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.auth0.jwt.interfaces.JWTVerifier;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
 public class JwtUtil {
 
+	private static final String SECRET_KEY = "Krakedev_Clave_Secreta_Para_Firma_JWT_2026_Secure";
+
+	private static final long EXPIRATION_TIME = 7200000;
+
+	public String generarToken(String username, String rol) {
+		return JWT.create().withSubject(username).withClaim("rol", rol).withIssuedAt(new Date())
+				.withExpiresAt(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+				.sign(Algorithm.HMAC256(SECRET_KEY));
+	}
+
+	public DecodedJWT validarToken(String token) {
+		Algorithm algoritmo = Algorithm.HMAC256(SECRET_KEY);
+		JWTVerifier verificador = JWT.require(algoritmo).build();
+		return verificador.verify(token);
+	}
 }
